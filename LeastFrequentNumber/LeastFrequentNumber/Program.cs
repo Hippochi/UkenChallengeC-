4b39fa424a2f029ef7b7acc5f1548c9879fc4bc7
Alex Dine <alexdine1996@gmail.com>
2022-04-21 01:49:59 -0400

    Added ClickToRun and location-independent  logic
    Changed from hard coding the file path to making it based on current directory

--- LeastFrequentNumber/LeastFrequentNumber/Program.cs
+++ LeastFrequentNumber/LeastFrequentNumber/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace LeastFrequentNumber
 {
     class Program
     {      
-        static void Main(string[] args)
+        static void Main(string[] args) //Creates the file path and filenames then passes these through to FileLoader
         {
             string[] filesToLoad = {"5.txt",
             "4.txt",
@@ -13,11 +14,12 @@ namespace LeastFrequentNumber
             "2.txt",
             "1.txt"
             };
-            FileLoader(filesToLoad, "H:/SelfProjects/UkenChallenge/UkenChallenge/LeastFrequentNumber/Files");
+            string directory = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory()))) + "\\files";
+            FileLoader(filesToLoad, directory);
             Console.ReadLine();
         }
 
-        static void WriteOutput(string fileName, int mostFrequent, int frequency)
+        static void WriteOutput(string fileName, int mostFrequent, int frequency) // Outputs the string in proper format from the results passed through as parameters
         {
             Console.WriteLine("File: " + fileName + ", Most Frequent Number: "
                 + mostFrequent + ", Repeated: " + frequency + " times");
@@ -25,8 +27,10 @@ namespace LeastFrequentNumber
 
         }
 
-        static int[] ArrayFromFile(string filename)
-        {
+        static int[] ArrayFromFile(string filename) //takes a file that uses linebreaks as seperation,
+                                                    //turns it into a string array that holds each line in its own string,
+                                                    //then creates an array of ints to hold the converted ints from the strings
+        { 
             System.IO.StreamReader reader =
                 new System.IO.StreamReader(filename);
 
@@ -45,9 +49,9 @@ namespace LeastFrequentNumber
             return result;
         }
 
-        static KeyValuePair<int,int> LeastFrequent(int[] array)
+        static KeyValuePair<int,int> LeastFrequent(int[] array) //Algorithm to count all of the array elements
         {
-            // Insert all elements in hash
+            // Hashes all the elements and counts their frequency
             Dictionary<int, int> hashTable =
                         new Dictionary<int, int>();
 
@@ -58,14 +62,13 @@ namespace LeastFrequentNumber
                 if (hashTable.ContainsKey(hashKey))
                 {
                     hashTable[hashKey]++;
-                    
                 }
                 else
                     hashTable.Add(hashKey, 1);
                 
             }
 
-            // find max frequency.
+            //sorts through the hash and compares KVPs to check for lowest frequency
             int max_count = int.MaxValue;
             KeyValuePair<int, int> result = new KeyValuePair<int, int>(0,-1);
             foreach (KeyValuePair<int,int> pair in hashTable)
@@ -75,7 +78,7 @@ namespace LeastFrequentNumber
                     max_count = pair.Value;
                     result = pair;
                 }
-                else if (max_count == pair.Value)
+                else if (max_count == pair.Value) // if there's a tie in frequency, chooses the KVP with the lowest value
                 {
                     if (result.Key > pair.Key)
                         result = pair;
@@ -85,11 +88,12 @@ namespace LeastFrequentNumber
             return result;
         }
 
-        static void FileLoader(string[] files, string path)
+        static void FileLoader(string[] files, string path) //iterates through each file, calculating the frequency of the elements,
+                                                            //then calls upon WriteOutput to put these values into a visible output
         {
             for (int i = 0; i <files.Length;i++)
             {
-                KeyValuePair<int, int> KVP = LeastFrequent(ArrayFromFile(path + "/"+ files[i]));
+                KeyValuePair<int, int> KVP = LeastFrequent(ArrayFromFile(path + "\\"+ files[i]));
                 WriteOutput(files[i], KVP.Key, KVP.Value);
                
             }
